1444efd77ec92d6b346d02c6666e339d72e4d06d
PavunRajTech <pavunraj.p@optisolbusiness.com>
2022-05-10 13:58:34 +0530

    Add Project

---
+++ TestAppFrame/TestAppFrame/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TestAppFrame
+//
+//  Created by Laptop-OBS-36 on 10/05/22.
+//
+
+import UIKit
+import TestUmbrallaFrame
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        Validator.CallMethod()
+        Validator.CallMethodTwo()
+        
+    }
+
+
+}
+
